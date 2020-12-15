1169f349612ba36ee981e65b442e90ca3c527ea1
Danniella Nasir <danniellanas@gmail.com>
2020-12-15 16:48:15 +0000

    pushing with webpack now

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  devtool: 'eval-source-map',
   entry: "./src/Index.js",
   output: {
     filename: "myBundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "To do list",
+    template : './public/index.html'
+  })],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port : 9010
+    contentBase: path.join(__dirname, "dist"),
+    port: 9010,
   },
   module: {
     rules: [
@@ -21,21 +30,18 @@ module.exports = {
           options: {
             presets: [
               "@babel/preset-env",
-               "@babel/preset-react",
-               {
-                'plugins': ['@babel/plugin-proposal-class-properties']
-              }
-              ],
+              "@babel/preset-react",
+              {
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            ],
           },
         },
       },
       {
-        test : /\.css$/,
-        use : [
-          {loader : 'style-loader'},
-          {loader : 'css-loader'}
-        ]
-      }
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
 };
